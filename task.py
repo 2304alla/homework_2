c5d565e820765fe58a5f91077ac046269ba2a99c
allakoval <2448572@gmail.com>
2023-01-06 13:52:54 +0500

    5

--- task.py
+++ task.py
@@ -32,21 +32,41 @@
 
 # - Для n=4 {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
 
-n=int(input())
-dict = {}
-sum=0
-for i in range(1,n+1):
-    dict[i] = round((1+1/i)**i,2)
+# n=int(input())
+# dict = {}
+# sum=0
+# for i in range(1,n+1):
+#     dict[i] = round((1+1/i)**i,2)
     
     
-for i in dict.values():
-    sum+=i    
+# for i in dict.values():
+#     sum+=i    
     
-print(dict) 
-print(sum)   
+# print(dict) 
+# print(sum)   
 
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # 4 Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+n=int(input())
+list_number=[i for i in range(-n,n+1)]
+print(list_number)
+
+
+file = open ("file.txt","r")
+
+
+for i in file:
+    print (int(i))
+    
+result=1
+
+for i in range(len(file)):
+    result*= list_number[file[i]]  
+print(result)    
+
+# list_number = list(map(strip,file))
+# print(list_number)
